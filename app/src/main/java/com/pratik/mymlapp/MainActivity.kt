2eebe79f41cd02cd622fc3f10ab843bfd56aed78
Pratik Desai <pratikdesai9701@gmail.com>
2022-08-06 14:11:40 -0400

    PDV | added permissions view to check for camera permissions

---
+++ app/src/main/java/com/pratik/mymlapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.pratik.mymlapp
+
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pratik.mymlapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+
+    override fun onBackPressed() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
